abc8ce8e9ac94ffee9ebc41c415b5d89ebce13d0
Shreya LR <shravyashreya26@gmail.com>
2026-02-19 15:51:04 +0530

    do

--- models.py
+++ models.py
@@ -9,4 +9,19 @@ class User(Base):
     password=Column(String)
     api_key=Column(String)
     username=Column(String)
-  
+class address(Base):
+    __tablename__="addresses"
+    id=Column(Integer,primary_key=True,index=True)
+    user_id=Column(Integer)
+    address=Column(String)
+    city=Column(String)
+    state=Column(String)
+    zip_code=Column(String)
+class order(Base):
+    __tablename__="orders"
+    id=Column(Integer,primary_key=True,index=True)
+    user_id=Column(Integer)
+    order_date=Column(DateTime,default=datetime.now())
+    total_amount=Column(Integer)
+    status=Column(String)
+   
